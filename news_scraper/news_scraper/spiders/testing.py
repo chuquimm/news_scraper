2cfcc2ee0896d852b6153c2fca74abe14f37b657
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2020-11-19 06:36:21 -0500

    testing quotes example

--- news_scraper/news_scraper/spiders/testing.py
+++ news_scraper/news_scraper/spiders/testing.py
@@ -5,10 +5,23 @@ import scrapy
 class TestingSpider(scrapy.Spider):
     name = 'testing'
     start_urls = ['http://quotes.toscrape.com/']
+    custom_settings = {
+        'FEED_URI': 'quotes.json',
+        'FEED_FORMAT': 'json',
+    }
 
     def parse(self, response):
-        print('*' * 10)
-        print('\n\n')
-        print(response.status, response.headers)
-        print('\n\n')
-        print('*' * 10)
+        title = response.xpath('//h1/a/text()').get()
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        tags = response.xpath(
+            '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall() # top 10
+
+        yield {
+            'title': title,
+            'quotes': quotes,
+            'tags': tags,
+        }
+
+        next_page = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse) # add relative url
