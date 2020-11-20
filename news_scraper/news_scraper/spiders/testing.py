aa915f81b80042c0fb973f0f85dcddd9fe92a195
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2020-11-20 06:27:59 -0500

    more testing custom_settings

--- news_scraper/news_scraper/spiders/testing.py
+++ news_scraper/news_scraper/spiders/testing.py
@@ -8,20 +8,45 @@ class TestingSpider(scrapy.Spider):
     custom_settings = {
         'FEED_URI': 'quotes.json',
         'FEED_FORMAT': 'json',
+        'CONCURRENT_REQUESTS': 24,
+        'MEMUSAGE_LIMIT_MB': 2048,
+        #'MEMUSAGE_NOTIFY_MAIL': ['SOME@EMAIL.COM'],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'NEWSSRAPER',
+        'FEED_EXPORT_ENCODING': 'utf-8'
     }
+
+    def parse_quotes(self, response, **kwargs):
+        if kwargs:
+            quotes = kwargs['quotes']
+            quotes.extend(response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall())
+
+            next_page = response.xpath(
+                '//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+            if next_page:
+                # add relative url
+                yield response.follow(next_page, callback=self.parse_quotes, cb_kwargs={'quotes': quotes})
+            else:
+                yield {'quotes': quotes}
 
     def parse(self, response):
         title = response.xpath('//h1/a/text()').get()
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
         tags = response.xpath(
-            '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall() # top 10
+            '//div[contains(@class, "tags-box")]//span[@class="tag-item"]/a/text()').getall()
+
+        # scrapy crawl testing -a top=1
+        top = getattr(self, 'top', None)
+        if top:
+            top = int(top)
+            tags = tags[:top]
 
         yield {
             'title': title,
-            'quotes': quotes,
             'tags': tags,
         }
 
         next_page = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
         if next_page:
-            yield response.follow(next_page, callback=self.parse) # add relative url
+            # add relative url
+            yield response.follow(next_page, callback=self.parse_quotes, cb_kwargs={'quotes': quotes})
